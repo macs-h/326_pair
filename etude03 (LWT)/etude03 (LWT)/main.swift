005017feadfdc0f5b2438ce86ceb69a5b8612fe7
Max Huang <git@maxhuang.nz>
2018-07-12 12:27:35 +1200

    English to Maori verb matching works. Can read in stdin and process each word as a token.

--- etude03 (LWT)/etude03 (LWT)/main.swift
+++ etude03 (LWT)/etude03 (LWT)/main.swift
@@ -10,19 +10,65 @@ import Foundation
 
 var noVerb : Bool = false
 var noPronoun : Bool = false
-var unknownVerb : String? = ""
-let pronounArray : [String] = ["i", "me", "you", "he", "she", "him", "her", "we", "us",
-                                "they", "them"]
-let verbArray : [String] = ["go", "going", "gone",
-                            "make", "making", "made",
-                            "see", "seeing", "seen",
-                            "want", "wanting", "wanted",
-                            "call", "calling", "called",
-                            "ask", "asking", "asked",
-                            "read", "reading",
-                            "learn", "learning", "learned"]
+var unknownVerb : String?
+let EnglishPronounArray : [String] = ["i", "me", "you", "he", "she", "him", "her", "we",
+                                      "us", "they", "them"]
+let EnglishVerbArray : [String] = [ "go", "going", "gone",
+                                    "make", "making", "made",
+                                    "see", "seeing", "seen",
+                                    "want", "wanting", "wanted",
+                                    "call", "calling", "called",
+                                    "ask", "asking", "asked",
+                                    "read", "reading", "read",
+                                    "learn", "learning", "learned"]
+let MaoriVerbArray : [String] =   [ "haere",
+                                    "hanga",
+                                    "kite",
+                                    "hiahia",
+                                    "karanga",
+                                    "pātai",
+                                    "pānui",
+                                    "ako"]
+
+var inputString : String = ""
 
 // API for checking tense.
+// Reads in lines of input till no further input.
+while let stdin = readLine() {
+    inputString = stdin.lowercased()
+    
+    let tagger = NSLinguisticTagger(tagSchemes: [.lexicalClass], options: 0)
+    tagger.string = inputString
+    let range = NSRange(location: 0, length: inputString.utf16.count)
+    let options: NSLinguisticTagger.Options = [.omitPunctuation, .omitWhitespace]
+    tagger.enumerateTags(in: range, unit: .word, scheme: .lexicalClass, options: options) { tag, tokenRange, _ in
+        if let tag = tag {
+            let word = (inputString as NSString).substring(with: tokenRange)
+            print("\(word): \(tag.rawValue)")
+            
+            if tag.rawValue.lowercased() == "verb" {
+                // Gives the corresponding Maori verb
+                if EnglishVerbArray.contains(word) {
+    //                print(EnglishVerbArray.index(of: word)! / 3)
+                    print(MaoriVerbArray[EnglishVerbArray.index(of: word)! / 3])
+                } else {
+                    noVerb = true
+                }
+            } else if tag.rawValue.lowercased() == "pronoun" {
+                if EnglishPronounArray.contains(word) {
+                    print(EnglishPronounArray.index(of: word)!)
+                } else {
+                    noPronoun = true
+                }
+            }
+            
+            
+        }
+    }
+    
+    
+}
+
 
 // for loop: loop through each word in the sentence.
     // CONVERT TO LOWER CASE!
@@ -39,3 +85,5 @@ let verbArray : [String] = ["go", "going", "gone",
     //} else if (noVerb) {
     //    print("Unknown verb \"\(String(describing: unknownVerb))\"")
     //}
+
+// use "readline()" to parse input from CLI
