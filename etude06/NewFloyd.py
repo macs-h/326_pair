7f42d0ece5a319342c8c59b5796e02e0ba3da24d
Max Huang <git@maxhuang.nz>
2018-09-24 17:50:21 +1200

    e6: Memoised previous numbers. Still not fast enough, but much faster rn

--- etude06/NewFloyd.py
+++ etude06/NewFloyd.py
@@ -2,123 +2,59 @@
 #
 
 import time
-
-valueLessThanOne = False
-valueAlreadyExists = False
-noLoop = False
-previousLoopNums = {}
+from functools import reduce
 
 def cycle_floyd ( f, x0, upperLimit):
-    global valueLessThanOne, noLoop, valueAlreadyExists
+    global noLoop
+
 
     tortoise = f ( x0 )
     hare = f ( tortoise )
+
+    if noLoop:
+        return 0,0
 
     while ( tortoise != hare ):#and not valueLessThanOne:
         tortoise = f ( tortoise )
         hare = f ( f ( hare ) )
 
-        if hare > upperLimit:
-        # if hare > 9000000:
-            noLoop = True
-            break
-        if valueAlreadyExists or valueLessThanOne:
-            noLoop = True
-            break
+        if noLoop or hare > 9000000:
+            return 0,0
 
     mu = 0
     tortoise = x0
 
-    if noLoop or valueLessThanOne or valueAlreadyExists:
-        return 0,0
-    else:
-        while ( tortoise != hare ):
-            tortoise = f ( tortoise )
-            hare = f ( hare )
-            mu = mu + 1
+    while ( tortoise != hare ):
+        tortoise = f ( tortoise )
+        hare = f ( hare )
+        mu = mu + 1
 
-        lam = 1
-        hare = f ( tortoise )
-        while ( tortoise != hare ):
-            hare = f ( hare )
-            lam = lam + 1
+    lam = 1
+    hare = f ( tortoise )
+    while ( tortoise != hare ):
+        hare = f ( hare )
+        lam = lam + 1
 
     return lam, mu
 
 #*****************************************************************************80
 
-def cycle_floyd_test02 (x0 , upperLimit) :
-    # global valueLessThanOne, valueAlreadyExists
+def f1 ( i ) :
+    global currentLoopNums, noLoopNums, noLoop
 
-    lam, mu = cycle_floyd ( f2, x0, upperLimit )
+    value = sum(factors(i))
 
-    # if valueLessThanOne:
-    #     lam = 0
+    # if value in noLoopNums or value in previousLoopNums or value <= 1:
+    if value in noLoopNums:
+        noLoop = True
 
-    return lam
+    currentLoopNums[value] = value
 
-#*****************************************************************************80
-
-def resetBools ():
-    global valueAlreadyExists, valueLessThanOne, noLoop
-    valueAlreadyExists = False
-    valueLessThanOne = False
-    noLoop = False
-
-def getTime(time_elapsed):
-    hours, rem = divmod(time_elapsed, 3600)
-    minutes, seconds = divmod(rem, 60)
-    return "{:0>2}:{:0>2}:{:05.2f}".format(int(hours),int(minutes),seconds)
-
-def cycle_floyd_test ( ):
-    global previousLoopNums
-    longestCycle = 0
-    numCycles = 0
-
-    start_time = time.time()
-
-    # startNum = 14310
-    # endNum = 14320
-    startNum = 2
-    endNum = 1000000
-
-    #? ----------------------------
-    #? Stop checking at hard limit of iteration range.
-    #? If loop hits '1', stop!
-    #? A number can only be in a loop once! - check list of previous loopy numbers. If already in list, then counts at same loop. Same number cannot be in two different loops at the same time.
-    #? ----------------------------
-
-    for i in range (startNum, endNum):
-        if i % 10000 == 0:
-            print("--- {}\tat: {} ---".format(getTime(time.time() - start_time), i))
-
-        # valueLessThanOne = False
-        # noLoop = False
-        resetBools()
-
-        cycleLen = cycle_floyd_test02(i, endNum)
-        if cycleLen != 0:
-            previousLoopNums[i] = i
-            numCycles += 1
-            # print("> i: {}  \t\b\blen: {}".format(i, cycleLen), end="")
-            # print("\tFactors: {}".format(factors(i)))
-            if cycleLen > longestCycle:
-                longestCycle = cycleLen
-
-
-    print("-----\nRange:\t{} to {}".format(startNum, endNum))
-    print("Time:\t{}".format(getTime(time.time() - start_time)))
-    print("Cycles:\t{}\nMax:\t{}\n-----".format(numCycles, longestCycle))
-
-
-    return
-
+    return value
 
 #*****************************************************************************80
 
 def factors(n):
-    from functools import reduce
-
     setOfFactors = set(
         reduce(
             list.__add__,
@@ -132,24 +68,65 @@ def factors(n):
     return setOfFactors
 
 #*****************************************************************************80
-array = []
-def f2 ( i ) :
-    global valueLessThanOne, valueAlreadyExists, previousLoopNums
 
-    value = sum(factors(i))
-    # print(value)
-    # array.append(value)
+def resetBools ():
+    global  noLoop
+    noLoop = False
 
-    if value <= 1:
-        valueLessThanOne = True
-    if value in previousLoopNums:
-        valueAlreadyExists = True
-
-    return value
-
+def getTime(time_elapsed):
+    hours, rem = divmod(time_elapsed, 3600)
+    minutes, seconds = divmod(rem, 60)
+    return "{:0>2}:{:0>2}:{:05.2f}".format(int(hours),int(minutes),seconds)
 
 #*****************************************************************************80
 
 if ( __name__ == '__main__' ):
 
-    cycle_floyd_test ( )
+    # noLoop = False
+
+    #&----------------------------------------------------------------------------80
+    #& Things to memoise:
+    #&
+    #& If number already seen or part of previously seen loop, stop.
+    #&----------------------------------------------------------------------------80
+    # previousLoopNums = {1}
+    currentLoopNums = {}
+    noLoopNums = {}
+
+    longestCycle = 0
+    numCycles = 0
+    cycleArray = []
+
+    start_time = time.time()
+
+    # startNum = 14310
+    # endNum = 14320
+    startNum = 2
+    endNum = 9000000
+
+    #? ----------------------------
+    #? Stop checking at hard limit of iteration range.
+    #? If loop hits '1', stop!
+    #? A number can only be in a loop once! - check list of previous loopy numbers. If already in list, then counts at same loop. Same number cannot be in two different loops at the same time.
+    #? ----------------------------
+
+    for i in range (startNum, endNum):
+        if i % 100000 == 0:
+            print("--- {}\tat: {} ---".format(getTime(time.time() - start_time), i))
+
+        # resetBools()
+        noLoop = False
+        cycleLen, tmp = cycle_floyd(f1, i, endNum)
+
+        noLoopNums.update(currentLoopNums)
+        currentLoopNums = {}
+
+        if cycleLen != 0:
+            numCycles += 1
+
+            if cycleLen > longestCycle:
+                longestCycle = cycleLen
+
+    print("-----\nRange:\t{} to {}".format(startNum, endNum))
+    print("Time:\t{}".format(getTime(time.time() - start_time)))
+    print("Cycles:\t{}\nMax:\t{}\n-----".format(numCycles, longestCycle))
