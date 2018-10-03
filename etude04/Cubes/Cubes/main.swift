22d4843fa6d5b30addadf63a04cdf977275386b7
Sam Paterson <>
2018-10-04 10:11:25 +1300

    Cubes report done

--- etude04/Cubes/Cubes/main.swift
+++ etude04/Cubes/Cubes/main.swift
@@ -8,8 +8,8 @@
 
 import Foundation
 
-func rotateRight(array: [[Int]]) -> [[Int]]{
-    print("before right rotation \(array)")
+func rotateRight(array: [[String]]) -> [[String]]{
+    //print("before right rotation \(array)")
     var cube = array
     let tempRow = [cube[0][2], cube[0][3]]
     cube[0][2] = cube[0][0]
@@ -23,19 +23,9 @@ func rotateRight(array: [[Int]]) -> [[Int]]{
     return cube
 }
 
-func rotateClockwise(layer:Int, array: [[Int]]) -> [[Int]]{
-    print("before right clockwise rotation \(array)")
-    let temp = array[layer][3]
-    var cube = array
-    cube[layer][3] = cube[layer][2]
-    cube[layer][2] = cube[layer][0]
-    cube[layer][0] = cube[layer][1]
-    cube[layer][1] = temp
-    return cube
-}
 
-func rotateUp(array: [[Int]]) -> [[Int]]{
-    print("before up rotation \(array)")
+func rotateUp(array: [[String]]) -> [[String]]{
+    //print("before up rotation \(array)")
     var cube = array
     let tempRow = [cube[0][0], cube[0][2]]
     cube[0][0] = cube[0][1]
@@ -50,7 +40,7 @@ func rotateUp(array: [[Int]]) -> [[Int]]{
 }
 
 
-func FoundUniqueCube(cubeLet: [[Int]], uniqueCombinations: [[[Int]]: Int])-> Bool{
+func FoundUniqueCube(cubeLet: [[String]], uniqueCombinations: [[[String]]: Int])-> Bool{
     var cube = cubeLet
     for _ in 0..<4{
         cube = rotateRight(array: cube)
@@ -65,79 +55,46 @@ func FoundUniqueCube(cubeLet: [[Int]], uniqueCombinations: [[[Int]]: Int])-> Boo
             }
         }
     }
-    print("\n\ncube:\t\(cube)\ncublet:\t\(cubeLet)\n\n")
     return true
 }
-var uniqueCombinations = [[[Int]]: Int]()
-//var cube = [[1,0,0,0],[0,0,0,0]]
-var cube = [[Int](repeating: 0, count: 4), [Int](repeating: 0, count: 4)]
-//for i in 0..<cube[0].count{
-//    for _ in 0..<4{
-//        if FoundUniqueCube(cubeLet: cube, uniqueCombinations: uniqueCombinations){
-//            //Uninque combination so add to dic
-//            print("------ unique cube---")
-//            uniqueCombinations[cube] = 1
-//        }
-//    }
-//    cube[0][i] = 1
-//}
+var uniqueCombinations = [[[String]]: Int]()
 
-var listOfCubes = [[[Int]]: Int]()
 
-//the problem ----- missing the [0,0,0,0] [1,0,0,0] ones etc
+var listOfCubes = [[[String]]]()
 
-for i in 0..<4{
-    print("> i: \(i)\tpt1 move")
-    for j in i..<5{
-        print(">> j: \(j)\tpt1 change index")
-        var tempCube = cube
-        if j>0{
-            tempCube[0][j-1] = 1
-        }
-        for k in 0..<4{
-            print(">>> k: \(k)\tpt2 move")
-            for n in k..<5{
-                print(">>>> n: \(n)\tpt2 change index")
-//                var temp1Cube = cube
-                var temp1Cube = tempCube
-                if n>0{
-                    temp1Cube[1][n-1] = 1
-                }
-//                if FoundUniqueCube(cubeLet: temp1Cube, uniqueCombinations: uniqueCombinations){
-//                    //Uninque combination so add to dic
-////                    print("------ unique cube---")
-//                    uniqueCombinations[temp1Cube] = 1
-//                }
-                print("cube: \(temp1Cube)")
-                if listOfCubes[temp1Cube] == nil{
-                    listOfCubes[temp1Cube] = listOfCubes.count
-                    print("---- found cube \(temp1Cube)")
-                }
-                print()
-            }
-//            cube[1][k] = 1
-            
-            tempCube[1][k] = 1
-            
-            print("====\n")
-        }
-        cube[1] = [Int](repeating: 0, count: 4)
+
+
+
+
+//count upto 255 in binary and convert to array (128 cubes)
+
+for num in 0...255{
+    let binaryString = String(num,radix: 2).map { String($0) }
+    var binaryArray = [String](repeatElement("0", count: 8))
+    var i = 7
+    var j = 0
+    for _ in 0..<binaryString.count{
+        binaryArray[i] = binaryString[j]
+        i -= 1
+        j += 1
     }
     
-    cube[0][i] = 1
-    
+    let bArray = [Array(binaryArray[0...3]),Array(binaryArray[4...7])]
+    //print(bArray)
+    listOfCubes.append(bArray)
 }
-print("cubes \(listOfCubes.count)")
 
-for (cu, pos) in listOfCubes{
+//print("cubes \(listOfCubes.count)")
+
+for cu in listOfCubes{
     if FoundUniqueCube(cubeLet: cu, uniqueCombinations: uniqueCombinations){
         //Uninque combination so add to dic
 //                    print("------ unique cube---")
-        uniqueCombinations[cu] = pos
+        uniqueCombinations[cu] = 0
     }
 }
 
-for d in uniqueCombinations{
-    print("dict: \(d)")
-}
-print("cube dict \(uniqueCombinations.count)")
+//for d in uniqueCombinations{
+//    print("dict: \(d)")
+//}
+print("Number of unique cubes \(uniqueCombinations.count)")
